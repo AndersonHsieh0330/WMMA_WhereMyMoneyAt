c45ae3b62dd017653bbe64cff3c7c08d476ec337
andersonhsieh <andy13579246810@gmail.com>
2021-11-08 21:50:03 +0800

    Initial commit

---
+++ src/main/java/com/AndersonHsieh/WMMA/WmmaApplication.java
@@ -0,0 +1,13 @@
+package com.AndersonHsieh.WMMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AndersonHsieh/WMMA/WmmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AndersonHsieh.WMMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
